1b07fc753c8e01c9972e801dcb085fededafd1df
vinaybvciq <vinay.b@commerceiq.ai>
2026-01-21 09:56:29 +0530

    added deployment fils

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Static app configuration - No backend proxy
 export default defineConfig({
   plugins: [react()],
-  base: '/xmlconverter/',
-  server: {
-    port: 4202,
-  },
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: true,
-  },
+  }
 });
